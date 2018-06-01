8bfccc282ffb23fa6f510ac8864082d322d6cfc4
tylerjamesmadsen <mad09001@byui.edu>
2018-06-01 13:52:14 -0600

    Added methods for calculating and displaying wood length and glass area.

--- GlazerCalcUWP-TylerMadsen/MainPage.xaml.cs
+++ GlazerCalcUWP-TylerMadsen/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -29,7 +30,78 @@ namespace GlazerCalcUWP_TylerMadsen
 
         private void quantitySlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
-            this.quantity.Text = this.quantitySlider.Value.ToString();
+            quantity.Text = quantitySlider.Value.ToString();
+        }
+
+        private void input_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (validateNumericInput((TextBox)sender) == false)
+            {
+                return;
+            }
+        }
+
+        private bool validateNumericInput(TextBox sender)
+        {
+            if (!Regex.IsMatch(sender.Text, @"^[1-9][0-9]{0,2}$"))
+            {
+                sender.Text = "";
+                return false;
+            }
+            return true;
+        }
+
+        private void btnOrderButton_Click(object sender, RoutedEventArgs e)
+        {
+            //woodLength.Text = CalculateWoodLength().ToString();
+            //glassArea.Text = CalculateGlassArea().ToString();
+            orderDate.Text = DateTime.Now.ToShortDateString();
+        }
+
+        private double CalculateWoodLength(int width, int height)
+        {
+            return 2 * (width + height) * 3.25;
+        }
+
+        private int CalculateGlassArea(int width, int height)
+        {
+            return 2 * (width + height);
+        }
+
+        private void width_TextChanged(Object sender, TextChangedEventArgs e)
+        {
+            if (validateNumericInput((TextBox)sender) == false)
+            {
+                return;
+            }
+
+            if (validateNumericInput(height) == true)
+            {
+                woodLength.Text =
+                    CalculateWoodLength(Int32.Parse(width.Text.ToString()), Int32.Parse(height.Text.ToString()))
+                    .ToString();
+                glassArea.Text =
+                CalculateGlassArea(Int32.Parse(width.Text.ToString()), Int32.Parse(height.Text.ToString()))
+                .ToString();
+            }
+        }
+
+        private void height_TextChanged(Object sender, TextChangedEventArgs e)
+        {
+            if (validateNumericInput((TextBox)sender) == false)
+            {
+                return;
+            }
+
+            if (validateNumericInput(height) == true)
+            {
+                woodLength.Text =
+                    CalculateWoodLength(Int32.Parse(width.Text.ToString()), Int32.Parse(height.Text.ToString()))
+                    .ToString();
+                glassArea.Text =
+                CalculateGlassArea(Int32.Parse(width.Text.ToString()), Int32.Parse(height.Text.ToString()))
+                .ToString();
+            }
         }
     }
 }
